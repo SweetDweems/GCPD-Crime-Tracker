6a06d4ea026c9c781cefd44cf08393ad901de0d6
Darien <dweems@andrew.cmu.edu>
2018-12-10 03:13:55 -0500

    Built officer dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
+    if logged_in?
+      @officer = current_user.officer
+      if current_user.role?(:officer)
+        officer_dash()
+      elsif current_user.role?(:chief)
+        chief_dash()
+      elsif current_user.role?(:commish)
+        commish_dash()
+      end
+    end
   end
 
   def about
@@ -13,4 +23,28 @@ class HomeController < ApplicationController
 
   def search
   end
+
+  private
+  def officer_dash
+    @current_assignments = @officer.assignments.current.chronological
+    officer_notes = []
+    for note in @officer.investigation_notes do
+      if note.investigation.date_closed.nil?
+        officer_notes << note
+      end
+    end
+    @officer_notes = officer_notes.sort_by{|n| n.date}.last(3)
+    recent_notes = []
+    for assignment in @current_assignments do
+      recent_notes += assignment.investigation.investigation_notes
+    end
+    @recent_notes = recent_notes.sort_by{|n| n.date}.last(3) - @officer_notes
+  end
+
+  def chief_dash
+  end
+
+  def commish_dash
+  end
+
 end
